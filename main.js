714dce63d70b3201ef2217441969296cf83eea6c
Beka Dvalishvili <48184459+peiraki@users.noreply.github.com>
2023-05-20 19:02:18 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+const dataAraay = [
+    {
+        userId: 1,
+        userName: 'სახელი 1',
+        userLastName: 'გვარი 1',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 20,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 2,
+        userName: 'სახელი 2',
+        userLastName: 'გვარი 2',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 30,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 3,
+        userName: 'სახელი 3',
+        userLastName: 'გვარი 3',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 35,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 4,
+        userName: 'სახელი 4',
+        userLastName: 'გვარი 4',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 29,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 5,
+        userName: 'სახელი 5',
+        userLastName: 'გვარი 5',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 49,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 6,
+        userName: 'სახელი 6',
+        userLastName: 'გვარი 6',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 31,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 7,
+        userName: 'სახელი 7',
+        userLastName: 'გვარი 7',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 31,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+]
+
+dataAraay.forEach(card => {
+    cards.innerHTML += 
+    `<div onclick="activeForm(this)" class="card">
+            <div class="idnumber">${card.userId}</div>
+            <img src="${card.userImage}" alt="${card.userName}">
+            <div class="name">${card.userName}</div>
+            <div class="surname">${card.userLastName}</div>
+            <div class="age">${card.userAge}<span>: Years Old</span></div>
+            <div class="adresses">Adresses:<span>${card.userAddresses}</span></div>
+    </div>
+    `
+
+    console.log(card)
+});
+
+
+function activeForm(card){
+    // if(document.querySelector('.active')){
+    //     document.querySelector('.active').classList.remove('active')
+    // }
+
+    card.classList.toggle('active')
+}
+
